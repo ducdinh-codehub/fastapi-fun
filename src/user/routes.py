94ef6a40513a757d9de218dcf77324aae7e285c8
duc.dinhanh10 <duc.dinhanh10@mobifone.vn>
2025-07-29 18:01:21 +0700

    - Adding create-user.

--- src/user/routes.py
+++ src/user/routes.py
@@ -1,7 +1,17 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 
+from user.models import CreateUserRequest
+from user.services import createUser
+
 routers = APIRouter()
 
 @routers.get("/users/", tags=["Users"])
 async def get_users():
     return {"message": "Get Users"}
+
+@routers.post("/users/create-user/", tags=["Users"])
+async def get_users(Item: CreateUserRequest):
+    rsp = createUser(Item)
+    print("rsp", rsp)
+    return {"message": "Get Users"}
+
