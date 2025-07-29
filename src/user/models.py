6e6b5ce191c56a6b3bddcb7840d78a8693c014e3
duc.dinhanh10 <duc.dinhanh10@mobifone.vn>
2025-07-29 18:01:06 +0700

    - Adding new model for user model.

--- src/user/models.py
+++ src/user/models.py
@@ -1,6 +1,7 @@
+from pydantic import BaseModel
 from sqlmodel import Field, SQLModel
 class User(SQLModel, table=True):
-    id: int = Field(primary_key=True, default=None, nullable=False, unique=True)
+    id: int | None = Field(primary_key=True, default=None, nullable=False, unique=True)
     name: str | None = None
     full_name: str | None = None
     email: str | None = None
@@ -9,3 +10,18 @@ class User(SQLModel, table=True):
     created_at: str
     updated_at: str
     image_avatar: str | None = None
+
+class CreateUserRequest(BaseModel):
+    name: str | None = None
+    full_name: str | None = None
+    email: str | None = None
+    phone: str | None = None
+    age: int
+    created_at: str
+    updated_at: str
+    image_avatar: str | None = None
+
+class CreateUserResponse(BaseModel):
+    user_id: int| None = None
+    message: str | None = None
+    code: int | None = None
