cdb7e3a2fb9db7a19708e3c06e5e96188d3fb26e
duc.dinhanh10 <duc.dinhanh10@mobifone.vn>
2025-07-30 17:50:47 +0700

    - Clean code.

--- src/config.py
+++ src/config.py
@@ -1,21 +1,7 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic_settings import SettingsConfigDict
 
-class Settings(BaseSettings):
-    app_name: str
-    admin_email: str
-    app_version: str
-    db_name_type_prefix: str
-    postgres_server: str
-    postgres_port: str
-    postgres_db: str
-    postgres_user: str
-    postgres_password: str
-    environment: str
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
-
-
+from models import CommonConfigs
+class Settings(CommonConfigs):
     model_config = SettingsConfigDict(env_file="../.env", env_file_encoding='utf-8')
 
 
